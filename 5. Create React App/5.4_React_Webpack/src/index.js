da29e86cc3bb3dba9f82535a7991b85e946e19cd
Quan Nguyen <quan02077@gmail.com>
2026-06-23 16:58:10 +0700

    Thử tạo project react bằng vite

--- 5. Create React App/5.4_React_Webpack/src/index.js
+++ 5. Create React App/5.4_React_Webpack/src/index.js
@@ -1,16 +1,14 @@
-import React from 'react' // nạp thư viện react
-import ReactDOM from 'react-dom' // nạp thư viện react-dom
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-// Tạo component App
 function App() {
     return (
         <div>
-            <h1>Xin chào anh em F8!</h1>
+            <h1>Xin chao anh em F8!</h1>
             <h2>Hello world!!!</h2>
         </div>
     )
 }
 
-// Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById('root'))
-
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
