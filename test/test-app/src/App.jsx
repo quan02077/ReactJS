aa9446f7f9f967bb1d6307acc827417f88293abd
Quan Nguyen <quan02077@gmail.com>
2026-06-24 12:01:23 +0700

    useEffect

--- test/test-app/src/App.jsx
+++ test/test-app/src/App.jsx
@@ -1,11 +1,14 @@
 import UseStateExample from "./use_State.jsx"
+import UseEffectExample from "./useEffect.jsx"
 import ToDoList from "./toDoList.jsx"
+import { use, useState } from "react"
 
 function App() {
+  const [show, setShow] = useState(false)
   return (
     <div className="App">
-      {/* <UseStateExample /> */}
-      <ToDoList />
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <UseEffectExample />}
     </div>
   )
 }
---
+++ test/test-app/src/useEffect.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+//1. useEffect(callback)
+//- Gọi callback mỗi khi component re-render
+//- Gọi callback sau khi component thêm element vào DOM
+//2. useEffect(callback, [])
+//Chỉ gọi callback 1 lần khi component được mounted
+//3. useEffect(callback, [deps])
+//Chỉ gọi callback khi dependency thay đổi
+
+//------
+//1. Callback luôn được gọi sau khi component được mounted
+const tabs = ['posts', 'comments', 'albums']
+function UseEffectExample() {
+    const [title, setTitle] = useState('')
+    const [posts, setPosts] = useState([])
+    const [type, setType] = useState('posts')
+
+    useEffect(() => {
+        // document.title = title
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then(res => res.json())
+            .then(posts => {
+                setPosts(posts)
+            })
+    }, [type])
+    return (
+        <div>
+            <input
+                type="text"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            {tabs.map(tab => (
+                <button
+                    key={tab}
+                    style={type === tab ? { color: 'white', backgroundColor: '#333' } : {}}
+                    onClick={() => setType(tab)}
+                >
+                    {tab}
+                </button>
+            ))}
+            <ul>
+                {
+                    posts.map(post => (
+                        <li key={post.id}>{post.title || post.name}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default UseEffectExample
